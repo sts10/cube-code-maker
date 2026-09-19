7551cfbc7953c79d612eeabed5c80bf13a6bd22d
Sam Schlinkert <sschlinkert@gmail.com>
2026-09-18 22:42:14 -0400

    simplifiesa nd hoepfully improves logic of assigning cutter number based on letter frequencies.

--- src/lib.rs
+++ src/lib.rs
@@ -16,10 +16,13 @@ pub fn make_map_for_starting_with_given_letter(
             let third_letter = name.chars().nth(2).unwrap();
             let second_plus_third_letter = format!("{second_letter}{third_letter}");
             second_and_third_letter_map
-                .entry(second_plus_third_letter)
+                .entry(second_plus_third_letter.clone())
                 .and_modify(|count| *count += 1)
                 .or_insert(1);
         }
+    }
+    if first_letter.to_lowercase() == "b" {
+        eprintln!("B map is: {:?}\n", second_and_third_letter_map);
     }
     second_and_third_letter_map
 }
@@ -43,39 +46,54 @@ pub fn make_map_for_fourth_and_fifth(names: &[String]) -> HashMap<String, usize>
 
 pub fn create_cutter_ranges(map: HashMap<String, usize>, start: usize, end: usize) -> Vec<usize> {
     let occurence_total: usize = map.values().sum();
-    let mut percentages: Vec<f64> = vec![];
+
+    // We don't want our cutter number to end in a 0, so
+    // we'll remove 7 numbers for ending in 0 (30, 40, 50, 60, 70, 80, 90)
+    let number_of_buckets_we_have_total = (end - start) - 7;
+    // This variable occurences_per_bucket_threshold is the number of second+third letter
+    // occurance that we want to put in each "bucket", e.g. 21, 22, 23... 99.
+    let occurences_per_bucket_threshold: usize = occurence_total / number_of_buckets_we_have_total; // would floor this if I could?
+
+    // We don't want our cutter number to end in a 0, so
+    // we'll remove 7 numbers for ending in 0 (30, 40, 50, 60, 70, 80, 90)
+    // let percentage_bucket_width: f64 = 1.0 / (end - start - 7) as f64;
+    let mut range_map: Vec<usize> = [].to_vec();
+    let mut number_of_the_bucket_we_are_assigning_currently: usize = start;
+    let mut this_buckets_current_total: usize = 0;
 
     let alphabet: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".to_string();
 
     for second_letter in alphabet.chars() {
         for third_letter in alphabet.chars() {
             let second_plus_third_letter = format!("{second_letter}{third_letter}");
-            match map.get(&second_plus_third_letter) {
-                Some(letter_pair_occurences) => percentages
-                    .push((*letter_pair_occurences as f64 / occurence_total as f64) as f64),
-                None => percentages.push(0.0 as f64),
+            let this_pairs_number_of_occurences = match map.get(&second_plus_third_letter) {
+                Some(letter_pair_occurences) => letter_pair_occurences,
+                None => &0, // never occurred. we'll use 0
             };
-        }
-    }
+            // What would this bucket look like if we added only have of this pair's occurences?
+            let this_bucket_total_after_half_this_addition = this_buckets_current_total
+                + (*this_pairs_number_of_occurences as f64 / 2.0).ceil() as usize;
 
-    // we'll roughly remove 9 numbers for ending in 0
-    let percentage_bucket_width: f64 = 1.0 / (end - start - 9) as f64;
-    let mut range_map: Vec<usize> = [].to_vec();
-    let mut running_percentage_total: f64 = 0.0;
-    let mut number_to_assign: usize = start;
-
-    for percentage in percentages {
-        range_map.push(number_to_assign);
-        running_percentage_total += percentage;
-        if running_percentage_total > percentage_bucket_width {
-            // this is a bit shoddy
-            running_percentage_total = running_percentage_total - percentage_bucket_width;
-            if number_to_assign < end {
-                number_to_assign += 1;
-                // if number ends in a 0, skip that number by adding 1
-                if number_to_assign.is_multiple_of(10) {
-                    number_to_assign += 1;
+            // if it doesn't overflow our current bucket...
+            if this_bucket_total_after_half_this_addition <= occurences_per_bucket_threshold {
+                // we'll add this pair to this bucket
+                this_buckets_current_total =
+                    this_buckets_current_total + this_pairs_number_of_occurences;
+                range_map.push(number_of_the_bucket_we_are_assigning_currently);
+            } else {
+                // But! if adding even just half of this pair's occurrences to this bucket
+                // would cause it to overflow, let's start a new bucket!
+                number_of_the_bucket_we_are_assigning_currently =
+                    number_of_the_bucket_we_are_assigning_currently + 1;
+                // if our new bucket would end in a zero, add 1 more in order to skip it
+                // (see comment above)
+                if number_of_the_bucket_we_are_assigning_currently.is_multiple_of(10) {
+                    number_of_the_bucket_we_are_assigning_currently =
+                        number_of_the_bucket_we_are_assigning_currently + 1;
                 }
+
+                this_buckets_current_total = 0;
+                range_map.push(number_of_the_bucket_we_are_assigning_currently);
             }
         }
     }
